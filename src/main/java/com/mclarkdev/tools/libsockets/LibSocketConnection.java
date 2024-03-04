b11634746614036312b7a6a818f7b2ae98feebcd
Matt Clark <matt@mclarkdev.com>
2024-03-04 01:31:11 -0500

    Logic changes.

--- src/main/java/com/mclarkdev/tools/libsockets/LibSocketConnection.java
+++ src/main/java/com/mclarkdev/tools/libsockets/LibSocketConnection.java
@@ -18,9 +18,15 @@ public class LibSocketConnection {
 	private final Socket socket;
 	private final String hostAddr;
 
-	private final Thread runner;
+	private final long connected;
 
 	private final BufferedReader reader;
+
+	private double rxBytes = 0;
+	private double rxMessages = 0;
+
+	private double txBytes = 0;
+	private double txMessages = 0;
 
 	public LibSocketConnection(LibSocketConnectionCallback callback, Socket socket) throws IOException {
 
@@ -41,33 +47,14 @@ public class LibSocketConnection {
 		// Get host address
 		this.hostAddr = socket.getRemoteSocketAddress().toString();
 
+		// Time socket was connected
+		this.connected = System.currentTimeMillis();
+
 		// Open input stream
 		this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-		// Create listener thread
-		this.runner = new Thread() {
-			public void run() {
-
-				setName(String.format("LibSocketConnection (%s)", hostAddr));
-
-				try {
-					String line;
-					while ((line = reader.readLine()) != null) {
-						callback.onMessage(LibSocketConnection.this, line);
-					}
-				} catch (Exception | Error e) {
-					callback.onDiconnect(LibSocketConnection.this, e);
-				}
-
-				shutdown();
-			}
-		};
-
-		// Call implemented callback
-		this.callback.onConnect(LibSocketConnection.this);
-
 		// Start thread
-		this.runner.start();
+		new Thread(worker).start();
 	}
 
 	public String getConnectionId() {
@@ -82,9 +69,39 @@ public class LibSocketConnection {
 		return socket;
 	}
 
+	public String getAddress() {
+		return hostAddr;
+	}
+
+	public long getTimeConnected() {
+		return connected;
+	}
+
+	public double getRxBytes() {
+		return rxBytes;
+	}
+
+	public double getRxMessages() {
+		return rxMessages;
+	}
+
+	public double getTxBytes() {
+		return txBytes;
+	}
+
+	public double getTxMessages() {
+		return txMessages;
+	}
+
 	public void write(byte[] bytes) throws IOException {
+
+		// Send the data
 		this.socket.getOutputStream().write(bytes);
 		this.socket.getOutputStream().flush();
+
+		// Hit the counters
+		this.txMessages += 1;
+		this.txBytes += bytes.length;
 	}
 
 	public void shutdown() {
@@ -97,4 +114,32 @@ public class LibSocketConnection {
 		} catch (IOException e) {
 		}
 	}
+
+	private final Runnable worker = new Runnable() {
+		public void run() {
+
+			Thread.currentThread().setName(//
+					String.format("LibSocketConnection (%s)", hostAddr));
+
+			// Call implemented callback
+			callback.onConnect(LibSocketConnection.this);
+
+			try {
+				String line;
+				while ((line = reader.readLine()) != null) {
+
+					// Hit the counters
+					rxMessages += 1;
+					rxBytes += line.length();
+
+					// Call implemented handler
+					callback.onMessage(LibSocketConnection.this, line);
+				}
+			} catch (Exception | Error e) {
+				callback.onDiconnect(LibSocketConnection.this, e);
+			}
+
+			shutdown();
+		}
+	};;
 }
